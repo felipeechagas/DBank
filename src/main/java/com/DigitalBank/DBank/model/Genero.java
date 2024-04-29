6f274b568c057471309a4102bb6e5149811248ef
Felipe Chagas <felipechagasferreira@gmail.com>
2024-04-29 15:48:10 -0300

    Fix Classe genero (DBank v.7.7.2)

--- src/main/java/com/DigitalBank/DBank/model/Genero.java
+++ src/main/java/com/DigitalBank/DBank/model/Genero.java
@@ -4,30 +4,24 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-/** Esta model representa o genero da pessoa.
- *
- * Uma pessoa somente pode possuir único genero. <p><br />
- *
- * @author FelipeChagas
- * @since 28/02/2023
- * @version 1.0
- */
-
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
-@Table(name = "tb_pessoa")
+@Table(name = "tb_genero")
 public class Genero {
+
+  private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   public static final char MASCULINO = 'M';
-  public static final char FEMININO = 'G';
+  public static final char FEMININO = 'F';
   private Character genero;
+
   public static String getNomeGenero(char genero) {
     switch (genero) {
       case MASCULINO:
@@ -57,16 +51,13 @@ public class Genero {
   public boolean isGeneroMasculino(Character genero) {
     return genero != null && genero == MASCULINO;
   }
+
   public boolean isGeneroFeminino(Character genero) {
-    return  genero != null && genero == FEMININO;
+    return genero != null && genero == FEMININO;
   }
+
   public boolean isEmpty() {
     return this.genero == null;
   }
-  public char getGenero() {
-    return genero;
-  }
-  public void setGenero(char genero) {
-    this.genero = genero;
-  }
+
 }
